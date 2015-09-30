1df7d92bc92da0838c8d0e881afb800464c3741e
Sara Colmenares <quehubosara@gmail.com>
2015-09-30 14:18:14 -0300

    just testing

--- teste.R
+++ teste.R
@@ -1,1 +1,6 @@
 # ola
+# Now we create a document README.md to write things on internet
+# an in help I can call for: Markdown Quick Reference
+# R Markdown is an easy-to-write plain text format for 
+# creating dynamic documents and reports. 
+# See Using R Markdown to learn more.
